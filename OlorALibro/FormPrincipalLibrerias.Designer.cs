e1356982f66a315f6916f492c48e6f35a40dfbae
arianxa <pro.arantxa.ordoyo@gmail.com>
2018-11-06 09:26:25 +0100

    FIll bien puesto

--- OlorALibro/FormPrincipalLibrerias.Designer.cs
+++ OlorALibro/FormPrincipalLibrerias.Designer.cs
@@ -30,18 +30,17 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipalLibrerias));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.imageListPrincipalLibrerias = new System.Windows.Forms.ImageList(this.components);
             this.buttonAñadir = new System.Windows.Forms.Button();
             this.buttonEliminar = new System.Windows.Forms.Button();
             this.buttonEditar = new System.Windows.Forms.Button();
+            this.dataGridViewPrincipalLibrerias = new System.Windows.Forms.DataGridView();
             this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NombreContacto = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombrecontacto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPrincipalLibrerias)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -53,21 +52,6 @@
             this.label1.Size = new System.Drawing.Size(281, 74);
             this.label1.TabIndex = 0;
             this.label1.Text = "Librerias";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Nombre,
-            this.Direccion,
-            this.Telefono,
-            this.NombreContacto});
-            this.dataGridView1.Location = new System.Drawing.Point(103, 201);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(622, 298);
-            this.dataGridView1.TabIndex = 1;
             // 
             // imageListPrincipalLibrerias
             // 
@@ -111,48 +95,61 @@
             this.buttonEditar.TabIndex = 5;
             this.buttonEditar.UseVisualStyleBackColor = true;
             // 
+            // dataGridViewPrincipalLibrerias
+            // 
+            this.dataGridViewPrincipalLibrerias.AllowUserToAddRows = false;
+            this.dataGridViewPrincipalLibrerias.AllowUserToDeleteRows = false;
+            this.dataGridViewPrincipalLibrerias.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewPrincipalLibrerias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPrincipalLibrerias.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.direccion,
+            this.telefono,
+            this.nombrecontacto});
+            this.dataGridViewPrincipalLibrerias.Location = new System.Drawing.Point(103, 223);
+            this.dataGridViewPrincipalLibrerias.Name = "dataGridViewPrincipalLibrerias";
+            this.dataGridViewPrincipalLibrerias.ReadOnly = true;
+            this.dataGridViewPrincipalLibrerias.RowTemplate.Height = 29;
+            this.dataGridViewPrincipalLibrerias.Size = new System.Drawing.Size(645, 262);
+            this.dataGridViewPrincipalLibrerias.TabIndex = 6;
+            // 
             // Nombre
             // 
-            this.Nombre.FillWeight = 300F;
-            this.Nombre.Frozen = true;
             this.Nombre.HeaderText = "Nombre";
             this.Nombre.Name = "Nombre";
-            this.Nombre.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.Nombre.Width = 125;
+            this.Nombre.ReadOnly = true;
             // 
-            // Direccion
+            // direccion
             // 
-            this.Direccion.HeaderText = "Dirección";
-            this.Direccion.Name = "Direccion";
-            this.Direccion.Width = 125;
+            this.direccion.HeaderText = "Direccion";
+            this.direccion.Name = "direccion";
+            this.direccion.ReadOnly = true;
             // 
-            // Telefono
+            // telefono
             // 
-            this.Telefono.HeaderText = "Telefono";
-            this.Telefono.Name = "Telefono";
-            this.Telefono.Width = 125;
+            this.telefono.HeaderText = "Telefono";
+            this.telefono.Name = "telefono";
+            this.telefono.ReadOnly = true;
             // 
-            // NombreContacto
+            // nombrecontacto
             // 
-            this.NombreContacto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.NombreContacto.DefaultCellStyle = dataGridViewCellStyle1;
-            this.NombreContacto.HeaderText = "Nombre Contacto";
-            this.NombreContacto.Name = "NombreContacto";
+            this.nombrecontacto.HeaderText = "Nombre de Contacto";
+            this.nombrecontacto.Name = "nombrecontacto";
+            this.nombrecontacto.ReadOnly = true;
             // 
             // FormPrincipalLibrerias
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(789, 511);
+            this.Controls.Add(this.dataGridViewPrincipalLibrerias);
             this.Controls.Add(this.buttonEditar);
             this.Controls.Add(this.buttonEliminar);
             this.Controls.Add(this.buttonAñadir);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "FormPrincipalLibrerias";
             this.Text = "FormPrincipalLibrerias";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPrincipalLibrerias)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,14 +158,14 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ImageList imageListPrincipalLibrerias;
         private System.Windows.Forms.Button buttonAñadir;
         private System.Windows.Forms.Button buttonEliminar;
         private System.Windows.Forms.Button buttonEditar;
+        private System.Windows.Forms.DataGridView dataGridViewPrincipalLibrerias;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NombreContacto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn direccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombrecontacto;
     }
 }
