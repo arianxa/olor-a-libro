1ec2857bd4ce66e2068e1d1e991ffa991fe289be
arianxa <pro.arantxa.ordoyo@gmail.com>
2018-11-08 14:34:49 +0100

    d

--- OlorALibro/FormModificarLibreria.cs
+++ OlorALibro/FormModificarLibreria.cs
@@ -9,8 +9,8 @@ namespace OlorALibro
 
     public partial class FormModificarLibreria : Form
     {
-         //List<Libreria> libreria = new List<Libreria>();
-       Libreria modlibreria;
+        //List<Libreria> libreria = new List<Libreria>();
+        Libreria modlibreria;
         public FormModificarLibreria()
         {
             InitializeComponent();
@@ -19,53 +19,58 @@ namespace OlorALibro
         {
             InitializeComponent();
             this.modlibreria = modlibreria;
-           //libreria = libreri;
-    public class Actividad : IEquatable<Actividad>
-    {
-        public string nombre { get; set; }
-        public DateTime fechaInicio { get; set; }
-        public DateTime fechaFinal { get; set; }
-        public List<string> categorias { get; set; }
-        public string descripcion { get; set; }
-        public List<string> librerias { get; set; }
-
-
-        // Saber quan una activitat es repeteix
-        public bool Equals(Actividad a) 
+            //libreria = libreri;
+        }
+        public class Actividad : IEquatable<Actividad>
         {
-            bool igual = false;
+            public string nombre { get; set; }
+            public DateTime fechaInicio { get; set; }
+            public DateTime fechaFinal { get; set; }
+            public List<string> categorias { get; set; }
+            public string descripcion { get; set; }
+            public List<string> librerias { get; set; }
 
-            if (a.nombre == this.nombre && a.fechaInicio == this.fechaInicio && a.fechaFinal == this.fechaFinal)
+
+            // Saber quan una activitat es repeteix
+            public bool Equals(Actividad a)
             {
-                igual = true;
+                bool igual = false;
+
+                if (a.nombre == this.nombre && a.fechaInicio == this.fechaInicio && a.fechaFinal == this.fechaFinal)
+                {
+                    igual = true;
+                }
+                return igual;
             }
-            return igual;
+            private void FormModificarLibreria_Load(object sender, EventArgs e)
+            {
+
+                textBoxnombreMod.Text = modlibreria.nombre;
+                textBoxtelefonoMod.Text = modlibreria.telefono.ToString();
+                textBoxdireccionMod.Text = modlibreria.direccion;
+                textBoxnombrecontactoMod.Text = modlibreria.nombreContacto;
+
+
+
+
+            }
+            private void buttonAceptarMod_Click(object sender, EventArgs e)
+            {
+
+                modlibreria.nombre = textBoxnombreMod.Text;
+                modlibreria.direccion = textBoxdireccionMod.Text;
+                modlibreria.telefono = int.Parse(textBoxtelefonoMod.Text);
+                modlibreria.nombreContacto = textBoxnombrecontactoMod.Text;
+                this.Close();
+
+
+
+            }
+
+
         }
-        private void FormModificarLibreria_Load(object sender, EventArgs e)
-        {
-           
-            textBoxnombreMod.Text = modlibreria.nombre;
-             textBoxtelefonoMod.Text = modlibreria.telefono.ToString();
-            textBoxdireccionMod.Text = modlibreria.direccion;
-            textBoxnombrecontactoMod.Text = modlibreria.nombreContacto;
-
-
-
-               
-        }
-        private void buttonAceptarMod_Click(object sender, EventArgs e)
-        {
-           
-            modlibreria.nombre = textBoxnombreMod.Text;
-            modlibreria.direccion = textBoxdireccionMod.Text;
-            modlibreria.telefono = int.Parse(textBoxtelefonoMod.Text);
-            modlibreria.nombreContacto = textBoxnombrecontactoMod.Text;
-            this.Close();
-
-
-
-        }
-
-      
     }
 }
+
+
+    
